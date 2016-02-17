2afc27e0b4ff302118cd97e1510896320575167d
Atit <atit.30187@gmail.com>
2016-02-16 21:25:36 -0800

    Plot 4 Code

--- Plot4.R
+++ Plot4.R
@@ -1,23 +1,23 @@
 library(ggplot2)
 
 # Loading thedata frames.
-nei <- readRDS("summarySCC_PM25.rds")
-scc <- readRDS("Source_Classification_Code.rds")
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
 
 # Subsetting the coal combustion related to NEI data
-combustionRelated <- grepl("comb", scc$scc.Level.One, ignore.case=TRUE)
-coalRelated <- grepl("coal", scc$scc.Level.Four, ignore.case=TRUE) 
+combustionRelated <- grepl("comb", SCC$SCC.Level.One, ignore.case=TRUE)
+coalRelated <- grepl("coal", SCC$SCC.Level.Four, ignore.case=TRUE) 
 coalCombustion <- (combustionRelated & coalRelated)
-combustionscc <- scc[coalCombustion,]$scc
-combustionnei <- nei[nei$scc %in% combustionscc,]
+combustionSCC <- SCC[coalCombustion,]$SCC
+combustionNEI <- NEI[NEI$SCC %in% combustionSCC,]
 
 png("plot4.png",width=480,height=480,units="px",bg="transparent")
 
-ggp <- ggplot(combustionnei,aes(factor(year),Emissions/10^5)) +
-  geom_bar(stat="identity",fill="steelblue",width=0.75) +
+ggp <- ggplot(combustionNEI,aes(factor(year),Emissions/10^5)) +
+  geom_bar(stat="identity",fill="grey",width=0.75) +
   theme_bw() +  guides(fill=FALSE) +
   labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
-  labs(title=expression("PM"[2.5]*" Coal Combustion Source Emissions Across US from 1999-2008"))
+  labs(title=expression("PM"[2.5]*" Coal Combustion Emissions Across US from 1999-2008"))
 
 print(ggp)
 
