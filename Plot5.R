3eeb2c2664521e9ecd7015c946fac895ff0811b1
Atit <atit.30187@gmail.com>
2016-02-16 21:32:48 -0800

    Plot 5 Code

--- Plot5.R
+++ Plot5.R
@@ -1,21 +1,21 @@
 library(ggplot2)
 
 # Loading the data frames.
-nei <- readRDS("summarySCC_PM25.rds")
-scc <- readRDS("Source_Classification_Code.rds")
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
 
 # Subsetting of the NEI data for vehicles
-vehicles <- grepl("vehicle", scc$scc.Level.Two, ignore.case=TRUE)
-vehiclesscc <- scc[vehicles,]$scc
-vehiclesnei <- nei[nei$scc %in% vehiclesscc,]
+vehicles <- grepl("vehicle", SCC$SCC.Level.Two, ignore.case=TRUE)
+vehiclesSCC <- SCC[vehicles,]$SCC
+vehiclesNEI <- NEI[NEI$SCC %in% vehiclesSCC,]
 
 # Subsetting the vehicles NEI data for Baltimore.
-baltimoreVehiclesnei <- vehiclesnei[vehiclesnei$fips=="24510",]
+baltimoreVehiclesNEI <- vehiclesNEI[vehiclesNEI$fips=="24510",]
 
 png("plot5.png",width=480,height=480,units="px",bg="transparent")
 
-ggp <- ggplot(baltimoreVehiclesnei,aes(factor(year),Emissions)) +
-  geom_bar(stat="identity",fill="steelblue",width=0.75) +
+ggp <- ggplot(baltimoreVehiclesNEI,aes(factor(year),Emissions)) +
+  geom_bar(stat="identity",fill="darkgreen",width=1) +
   theme_bw() +  guides(fill=FALSE) +
   labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
   labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore from 1999-2008"))
