f6bfa3bdfd445f8d4606c787f0b8a1992d8fce79
Atit <atit.30187@gmail.com>
2016-02-16 19:23:08 -0800

    Update

--- Plot1.R
+++ Plot1.R
@@ -6,14 +6,11 @@ scc <- readRDS("Source_Classification_Code.rds")
 # Aggregate (sum) total emissions by year
 aggTotals <- aggregate(Emissions ~ year,NEI, sum)
 
+
 png("plot1.png",width=480,height=480,units="px",bg="transparent")
 
 barplot(
-  (aggTotals$Emissions)/10^6,
-  names.arg=aggTotals$year,
-  xlab="Year",
-  ylab="PM2.5 Emissions (10^6 Tons)",
-  main="Total PM2.5 Emissions From All US Sources"
+  (aggTotals$Emissions)/10^6, names.arg=aggTotals$year, xlab="Year", ylab="PM2.5 Emissions (10^6 Tons)", main="Total PM2.5 Emissions From All US Sources"
 )
 
 dev.off()
