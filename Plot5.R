46842649c11ec389d0e0816cb5d6527b034d216d
Atit <atit.30187@gmail.com>
2016-02-16 21:35:20 -0800

    Plot 5 Code

--- Plot5.R
+++ Plot5.R
@@ -15,7 +15,7 @@ baltimoreVehiclesNEI <- vehiclesNEI[vehiclesNEI$fips=="24510",]
 png("plot5.png",width=480,height=480,units="px",bg="transparent")
 
 ggp <- ggplot(baltimoreVehiclesNEI,aes(factor(year),Emissions)) +
-  geom_bar(stat="identity",fill="darkgreen",width=1) +
+  geom_bar(stat="identity",fill="darkgreen",width=0.5) +
   theme_bw() +  guides(fill=FALSE) +
   labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
   labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore from 1999-2008"))
